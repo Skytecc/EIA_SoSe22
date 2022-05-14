46b79f3937e3c1be2a06db5b110f135d865fdf7f
Skytecc <101884474+Skytecc@users.noreply.github.com>
2022-05-14 18:21:51 +0200

    ADD drumsounds

--- Aufgaben/A08/drumPadscript.js
+++ Aufgaben/A08/drumPadscript.js
@@ -1,8 +1,26 @@
 var sound01 = new Audio('DrumPadSounds/A.mp3');
-sound01.play();
-var audio = [];
-sound01[0] = 'sound01';
-window.addEventListener("load", function soundSample() {
-    document.querySelector(".button1").addEventListener('click', function () { sound01[0]; });
+var sound02 = new Audio('DrumPadSounds/C.mp3');
+var sound03 = new Audio('DrumPadSounds/F.mp3');
+var sound04 = new Audio('DrumPadSounds/G.mp3');
+var sound05 = new Audio('DrumPadSounds/hihat.mp3');
+var sound06 = new Audio('DrumPadSounds/kick.mp3');
+var sound07 = new Audio('DrumPadSounds/laugh-1.mp3');
+var sound08 = new Audio('DrumPadSounds/laugh-2.mp3');
+var sound09 = new Audio('DrumPadSounds/snare.mp3');
+var sounds = [];
+new Audio('DrumPadSounds/A.mp3')[0] = 'musik01';
+function soundSample(sound) {
+    sound.play();
+}
+window.addEventListener("load", function () {
+    document.querySelector(".button1").addEventListener("click", function () { soundSample(sound01); });
+    document.querySelector(".button2").addEventListener("click", function () { soundSample(sound02); });
+    document.querySelector(".button3").addEventListener("click", function () { soundSample(sound03); });
+    document.querySelector(".button4").addEventListener("click", function () { soundSample(sound04); });
+    document.querySelector(".button5").addEventListener("click", function () { soundSample(sound05); });
+    document.querySelector(".button6").addEventListener("click", function () { soundSample(sound06); });
+    document.querySelector(".button7").addEventListener("click", function () { soundSample(sound07); });
+    document.querySelector(".button8").addEventListener("click", function () { soundSample(sound08); });
+    document.querySelector(".button9").addEventListener("click", function () { soundSample(sound09); });
 });
 //# sourceMappingURL=drumPadscript.js.map
--- Aufgaben/A08/drumPadscript.ts
+++ Aufgaben/A08/drumPadscript.ts
@@ -1,9 +1,30 @@
   var sound01:HTMLAudioElement = new Audio('DrumPadSounds/A.mp3');
-  sound01.play();
+  var sound02:HTMLAudioElement = new Audio('DrumPadSounds/C.mp3');
+  var sound03:HTMLAudioElement = new Audio('DrumPadSounds/F.mp3');
+  var sound04:HTMLAudioElement = new Audio('DrumPadSounds/G.mp3');
+  var sound05:HTMLAudioElement = new Audio('DrumPadSounds/hihat.mp3');
+  var sound06:HTMLAudioElement = new Audio('DrumPadSounds/kick.mp3');
+  var sound07:HTMLAudioElement = new Audio('DrumPadSounds/laugh-1.mp3');
+  var sound08:HTMLAudioElement = new Audio('DrumPadSounds/laugh-2.mp3');
+  var sound09:HTMLAudioElement = new Audio('DrumPadSounds/snare.mp3');
 
-  var audio:HTMLAudioElement [] = [];
-  sound01 [0] = 'sound01';
+  var sounds:HTMLAudioElement [] = [];
+  new Audio('DrumPadSounds/A.mp3') [0] = 'musik01';
 
-window.addEventListener("load", function soundSample () {
-    document.querySelector(".button1").addEventListener('click', function(){sound01 [0]});
+
+  function soundSample (sound:HTMLAudioElement) {
+    sound.play();
+  }
+
+window.addEventListener("load", function() {
+    document.querySelector(".button1").addEventListener("click", function() {soundSample (sound01)} );
+    document.querySelector(".button2").addEventListener("click", function() {soundSample (sound02)} );
+    document.querySelector(".button3").addEventListener("click", function() {soundSample (sound03)} );
+    document.querySelector(".button4").addEventListener("click", function() {soundSample (sound04)} );
+    document.querySelector(".button5").addEventListener("click", function() {soundSample (sound05)} );
+    document.querySelector(".button6").addEventListener("click", function() {soundSample (sound06)} );
+    document.querySelector(".button7").addEventListener("click", function() {soundSample (sound07)} );
+    document.querySelector(".button8").addEventListener("click", function() {soundSample (sound08)} );
+    document.querySelector(".button9").addEventListener("click", function() {soundSample (sound09)} );
 })
+
