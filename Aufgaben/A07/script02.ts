7690e93a2d2b9db6ddd6e0c95d40c77c6f7fc4d8
Skytecc <101884474+Skytecc@users.noreply.github.com>
2022-05-14 18:23:31 +0200

    Add

--- Aufgaben/A07/script02.ts
+++ Aufgaben/A07/script02.ts
@@ -30,29 +30,37 @@ var swEU : number = Math.round((sweden2022/eu2022) * 100);
 var swWR : number = Math.round((sweden2022/sweden2014 -1) *100);
 var swWRnr : number = Math.round(sweden2022-sweden2014);
 
+var ger : string = "#germany";
+var fra : string =  "#france";
+var ita : string = "#italy";
+var swe : string = "#sweden";
+
+var landbefore : string = "#france";
+
 
 // 07 Aufgabe
 
-
+window.addEventListener("load",function() {
 // EU Sterne
 
 function changeNumberEU() {
     document.querySelector(".euZahl").innerHTML = "Einwohnerzahl in der europäischen Union: " + eu2022.toString() + "Mio";
 }
 
-function changeNumber(number2022:number, numberEU:number,numberWR:number, numberWRnr:number) {
+function changeNumber(number2022:number, numberEU:number,numberWR:number, numberWRnr:number, land:string) {
     document.querySelector(".einwohnerGS").innerHTML = number2022.toString() + "Mio";
     document.querySelector(".vglEU").innerHTML = numberEU.toString() + "%";
     document.querySelector(".wr2014").innerHTML = numberWR.toString() + "%";
     document.querySelector(".wrDifference").innerHTML = numberWRnr.toString() + "Mio";
     document.querySelector(".chart").setAttribute("style", "height:" + numberEU + "%");
+    document.querySelector(land).classList.add("active");
+
 }
 
-window.addEventListener("load", function() {
-    document.querySelector(".germany").addEventListener("click",function() { changeNumber (germany2022, grEU, grWR, grWRnr)} );
-    document.querySelector(".france").addEventListener("click",function() { changeNumber (france2022, frEU, frWR, frWRnr)} );
-    document.querySelector(".italy").addEventListener("click",function() { changeNumber (italy2022, itEU, itWR, itWRnr)} );
-    document.querySelector(".sweden").addEventListener("click",function() { changeNumber (sweden2022, swEU, swWR, swWRnr)} );
+    document.querySelector(".germany").addEventListener("click",function() { changeNumber (germany2022, grEU, grWR, grWRnr, ger)} );
+    document.querySelector(".france").addEventListener("click",function() { changeNumber (france2022, frEU, frWR, frWRnr, fra)} );
+    document.querySelector(".italy").addEventListener("click",function() { changeNumber (italy2022, itEU, itWR, itWRnr, ita)} );
+    document.querySelector(".sweden").addEventListener("click",function() { changeNumber (sweden2022, swEU, swWR, swWRnr, swe)} );
     document.querySelector(".stars").addEventListener('click', changeNumberEU); 
 
 })
