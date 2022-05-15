bb43233669ae0843eb0f9d06de954d7ec37686e1
Skytecc <101884474+Skytecc@users.noreply.github.com>
2022-05-15 18:18:02 +0200

    Add fix

--- Aufgaben/A08/drumPadscript.js
+++ Aufgaben/A08/drumPadscript.js
@@ -7,20 +7,19 @@ var sound06 = new Audio('DrumPadSounds/kick.mp3');
 var sound07 = new Audio('DrumPadSounds/laugh-1.mp3');
 var sound08 = new Audio('DrumPadSounds/laugh-2.mp3');
 var sound09 = new Audio('DrumPadSounds/snare.mp3');
-var sounds = [];
-new Audio('DrumPadSounds/A.mp3')[0] = 'musik01';
+var sounds = [sound01, sound02, sound03, sound04, sound04, sound05, sound06, sound07, sound08, sound09];
 function soundSample(sound) {
     sound.play();
 }
 window.addEventListener("load", function () {
-    document.querySelector(".button1").addEventListener("click", function () { soundSample(sound01); });
-    document.querySelector(".button2").addEventListener("click", function () { soundSample(sound02); });
-    document.querySelector(".button3").addEventListener("click", function () { soundSample(sound03); });
-    document.querySelector(".button4").addEventListener("click", function () { soundSample(sound04); });
-    document.querySelector(".button5").addEventListener("click", function () { soundSample(sound05); });
-    document.querySelector(".button6").addEventListener("click", function () { soundSample(sound06); });
-    document.querySelector(".button7").addEventListener("click", function () { soundSample(sound07); });
-    document.querySelector(".button8").addEventListener("click", function () { soundSample(sound08); });
-    document.querySelector(".button9").addEventListener("click", function () { soundSample(sound09); });
+    document.querySelector(".button1").addEventListener("click", function () { soundSample(sounds[0]); });
+    document.querySelector(".button2").addEventListener("click", function () { soundSample(sounds[1]); });
+    document.querySelector(".button3").addEventListener("click", function () { soundSample(sounds[2]); });
+    document.querySelector(".button4").addEventListener("click", function () { soundSample(sounds[3]); });
+    document.querySelector(".button5").addEventListener("click", function () { soundSample(sounds[4]); });
+    document.querySelector(".button6").addEventListener("click", function () { soundSample(sounds[5]); });
+    document.querySelector(".button7").addEventListener("click", function () { soundSample(sounds[6]); });
+    document.querySelector(".button8").addEventListener("click", function () { soundSample(sounds[7]); });
+    document.querySelector(".button9").addEventListener("click", function () { soundSample(sounds[8]); });
 });
 //# sourceMappingURL=drumPadscript.js.map
