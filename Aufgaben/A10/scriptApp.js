891e7b28fc1b3b752640fce6839897dce7f65b2e
Skytecc <101884474+Skytecc@users.noreply.github.com>
2022-06-08 00:31:23 +0200

    Add

--- Aufgaben/A10/scriptApp.js
+++ Aufgaben/A10/scriptApp.js
@@ -1,2 +1,12 @@
-const taskInput = document.querySelector(".myInput");
+const taskInput = document.getElementById("myInput");
+taskInput.addEventListener("keyup", i => {
+    const value = taskInput.value.trim();
+    if (i.key == "Enter" && value) {
+        let todos = localStorage.getItem(".applist");
+        if (!todos) {
+            todos[] = [];
+        }
+        console.log(value);
+    }
+});
 //# sourceMappingURL=scriptApp.js.map
--- Aufgaben/A10/scriptApp.ts
+++ Aufgaben/A10/scriptApp.ts
@@ -1,1 +1,15 @@
-const taskInput = document.querySelector(".myInput");
+const taskInput = document.getElementById("myInput") as HTMLInputElement;
+
+
+
+taskInput.addEventListener("keyup", i => {
+    const value : string = taskInput.value.trim(); 
+    if(i.key == "Enter" && value) {
+        let todos = localStorage.getItem(".applist");
+        if (!todos) {
+            todos[] = [];
+        }
+
+        console.log(value);
+    }
+})
