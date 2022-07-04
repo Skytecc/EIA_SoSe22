237e116eed216432da8a5cd1d4971cb45a149efe
Skytecc <101884474+Skytecc@users.noreply.github.com>
2022-07-04 02:45:52 +0200

    add

--- Aufgaben/AKlausur/test.js
+++ Aufgaben/AKlausur/test.js
@@ -3,4 +3,13 @@ while (a < 5) {
     a += a;
     console.log(a);
 }
+// Array Iterieren und höchstes Value bekommen
+let test01 = [85, 89, 102, 222, 55, 110];
+var highestNumber = test01[0];
+for (var i = 0; i < test01[i]; i++) {
+    if (test01[i] > highestNumber) {
+        highestNumber = test01[i];
+    }
+}
+console.log(highestNumber);
 //# sourceMappingURL=test.js.map
--- Aufgaben/AKlausur/test.ts
+++ Aufgaben/AKlausur/test.ts
@@ -4,3 +4,19 @@ while (a < 5) {
 
  console.log(a);
 }
+
+// Array Iterieren und höchstes Value bekommen
+
+let test01: number[] = [85,89,102,222,55,110];
+
+var highestNumber: number = test01[0];
+
+for (var i: number = 0; i < test01[i] ; i++) {
+
+    if(test01[i] > highestNumber) {
+        highestNumber = test01[i];
+    }   
+
+}
+
+console.log(highestNumber);
